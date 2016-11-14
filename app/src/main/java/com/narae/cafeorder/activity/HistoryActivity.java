848c735abd8c1b43f40a1dad828afc58ccbae2b6
Sung-Yeol Yun <existmaster@naver.com>
2016-11-14 21:47:25 +0900

    주문현황 레이아웃 제작

--- app/src/main/java/com/narae/cafeorder/activity/HistoryActivity.java
+++ app/src/main/java/com/narae/cafeorder/activity/HistoryActivity.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import com.narae.cafeorder.R;
 import com.narae.cafeorder.fragments.OneFragment;
+import com.narae.cafeorder.fragments.ThreeFragment;
 import com.narae.cafeorder.fragments.TwoFragment;
 
 public class HistoryActivity extends AppCompatActivity {
@@ -41,7 +42,7 @@ public class HistoryActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new OneFragment(), "주문현황");
+        adapter.addFragment(new ThreeFragment(), "주문현황");
         adapter.addFragment(new TwoFragment(), "과거내역");
         viewPager.setAdapter(adapter);
     }
--- app/src/main/java/com/narae/cafeorder/fragments/ThreeFragment.java
+++ app/src/main/java/com/narae/cafeorder/fragments/ThreeFragment.java
@@ -2,14 +2,19 @@ package com.narae.cafeorder.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.narae.cafeorder.R;
+import com.narae.cafeorder.adapter.CurrentListAdapter;
 
 
 public class ThreeFragment extends Fragment{
+
+    CurrentListAdapter adapter ;
 
     public ThreeFragment() {
         // Required empty public constructor
@@ -24,7 +29,19 @@ public class ThreeFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_three, container, false);
+        View view = inflater.inflate(R.layout.fragment_three, container, false);
+
+        // Adapter 생성 및 Adapter 지정.
+        adapter = new CurrentListAdapter() ;
+
+        adapter.addItem("아메리카노", "Tall/Hot/2건/6000원") ;
+        adapter.addItem("카페라떼", "Tall/Iced/1건/4000원") ;
+
+        ListView listview = (ListView)view.findViewById(R.id.statuslist);
+        listview.setAdapter(adapter);
+
+        return view;
+
     }
 
 }
