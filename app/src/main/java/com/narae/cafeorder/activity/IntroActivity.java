661e622d5becdf944a6870acfe81d41baa0251f3
jo-narae <nare6004@gmail.com>
2016-11-30 23:59:29 +0900

    아이디 중복체크 구현

--- app/src/main/java/com/narae/cafeorder/activity/IntroActivity.java
+++ app/src/main/java/com/narae/cafeorder/activity/IntroActivity.java
@@ -3,23 +3,39 @@ package com.narae.cafeorder.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.narae.cafeorder.R;
 import com.narae.cafeorder.database.DBManager;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class IntroActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button buttonIntro;
+    private Button buttonidcheck;
     private TextView user_id;
     private TextView user_name;
     private TextView user_password;
     private TextView password_check;
     DBManager manager;
+
+    private boolean idCheckFlag = false;
+    private boolean idValidationFlag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +43,7 @@ public class IntroActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_intro);
 
         buttonIntro = (Button) findViewById(R.id.buttonintro);
+        buttonidcheck = (Button) findViewById(R.id.buttonidcheck);
         user_id = (TextView) findViewById(R.id.user_id);
         user_name = (TextView) findViewById(R.id.user_name);
         user_password = (TextView) findViewById(R.id.user_password);
@@ -36,6 +53,7 @@ public class IntroActivity extends AppCompatActivity implements View.OnClickList
         userCheck(); //디바이스 내 회원 정보가 있는지 체크
 
         buttonIntro.setOnClickListener(this);
+        buttonidcheck.setOnClickListener(this);
     }
 
     @Override
@@ -47,7 +65,64 @@ public class IntroActivity extends AppCompatActivity implements View.OnClickList
                     break;
                 }
                 break;
+            case R.id.buttonidcheck:
+                idOverlapCheck();
+                break;
         }
+    }
+
+    /**
+     * 아이디 중복체크(서버통신)
+     */
+    private void idOverlapCheck() {
+        // 아이디 중복체크를 위해서 아이디를 입력 받는다.
+        if(user_id.getText().toString().equals("")){
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "아이디를 입력해주세요.", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM, 0, 200);
+            toast.show();
+            return;
+        }
+        // 아이디를 입력 받은 후 서버 통신을 한다.
+        String url = getString(R.string.server_url) + "/users/" + user_id.getText().toString();
+        sendRequest(url);
+        Log.d("idCheckFlag : ", String.valueOf(idCheckFlag));
+        if(idCheckFlag) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "아이디가 존재합니다.", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM, 0, 200);
+            toast.show();
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "사용 가능한 아이디입니다.", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM, 0, 200);
+            toast.show();
+        }
+    }
+
+    /**
+     * 서버통신
+     * @param url
+     */
+    private void sendRequest(String url) {
+        JsonObjectRequest socRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // 아이디가 있을 경우 이 로직 실행함
+                idCheckFlag = false;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // 아이디가 없을 경우 이 로직 실행함
+                idCheckFlag = true;
+            }
+        });
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this.getApplicationContext());
+        requestQueue.add(socRequest);
+
     }
 
     /**
@@ -56,35 +131,42 @@ public class IntroActivity extends AppCompatActivity implements View.OnClickList
     private boolean validationCheck() {
         if(user_id.getText().toString().equals("")){
             Toast toast = Toast.makeText(getApplicationContext(),
-                    "아이디를 입력해주세요.", Toast.LENGTH_LONG);
+                    "아이디를 입력해주세요.", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.BOTTOM, 0, 200);
+            toast.show();
+            return false;
+        }
+        if(!idValidationFlag) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "아이디 중복체크를 해주세요.", Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.BOTTOM, 0, 200);
             toast.show();
             return false;
         }
         if(user_name.getText().toString().equals("")){
             Toast toast = Toast.makeText(getApplicationContext(),
-                    "이름 또는 별명을 입력해주세요.", Toast.LENGTH_LONG);
+                    "이름 또는 별명을 입력해주세요.", Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.BOTTOM, 0, 200);
             toast.show();
             return false;
         }
         if(user_password.getText().toString().equals("")){
             Toast toast = Toast.makeText(getApplicationContext(),
-                    "비밀번호를 입력해주세요.", Toast.LENGTH_LONG);
+                    "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.BOTTOM, 0, 200);
             toast.show();
             return false;
         }
         if(password_check.getText().toString().equals("")){
             Toast toast = Toast.makeText(getApplicationContext(),
-                    "비밀번호를 재입력해주세요.", Toast.LENGTH_LONG);
+                    "비밀번호를 재입력해주세요.", Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.BOTTOM, 0, 200);
             toast.show();
             return false;
         }
         if(!user_password.getText().toString().equals(password_check.getText().toString())){
             Toast toast = Toast.makeText(getApplicationContext(),
-                    "비밀번호와 비밀번호 재입력이\n일치하지 않습니다.\n다시 확인해주세요.", Toast.LENGTH_LONG);
+                    "비밀번호와 비밀번호 재입력이\n일치하지 않습니다.\n다시 확인해주세요.", Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.BOTTOM, 0, 200);
             toast.show();
             return false;
