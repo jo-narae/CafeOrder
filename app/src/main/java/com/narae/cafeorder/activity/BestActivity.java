c5bbfcbffa3ebe57d41087ffaecce4856c076591
jo-narae <nare6004@gmail.com>
2016-12-07 22:00:57 +0900

    베스트5 로직 구현 완료

--- app/src/main/java/com/narae/cafeorder/activity/BestActivity.java
+++ app/src/main/java/com/narae/cafeorder/activity/BestActivity.java
@@ -10,18 +10,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.narae.cafeorder.R;
 import com.narae.cafeorder.best.BestMenuListViewAdapter;
-import com.narae.cafeorder.best.BestMenuListViewItem;
 import com.narae.cafeorder.database.DBManager;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class BestActivity extends AppCompatActivity {
 
-    private View seletedItem;
+    BestMenuListViewAdapter adapter;
     private Toolbar toolbar;
 
     private LayerDrawable mCartMenuIcon;
@@ -41,7 +49,6 @@ public class BestActivity extends AppCompatActivity {
 
 
         final ListView listview ;
-        BestMenuListViewAdapter adapter;
 
         // Adapter 생성
         adapter = new BestMenuListViewAdapter() ;
@@ -50,22 +57,7 @@ public class BestActivity extends AppCompatActivity {
         listview = (ListView) findViewById(R.id.lv_best);
         listview.setAdapter(adapter);
 
-        // 첫 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.rank1), ContextCompat.getDrawable(this, R.drawable.americano),
-                "아메리카노", "300건") ;
-        // 두 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.rank2), ContextCompat.getDrawable(this, R.drawable.americano),
-                "카푸치노", "200건") ;
-        // 세 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.rank3), ContextCompat.getDrawable(this, R.drawable.americano),
-                "얼 그레이 티", "100건") ;
-
-        // 네 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.rank4), ContextCompat.getDrawable(this, R.drawable.americano),
-                "카페 라떼", "70건") ;
-        // 다섯 번째 아이템 추가.
-        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.rank5), ContextCompat.getDrawable(this, R.drawable.americano),
-                "카페 모카", "20건") ;
+        top5ListData();
 
         manager = new DBManager(this);
         mCartCount = manager.cartTotalCount();
@@ -80,6 +72,47 @@ public class BestActivity extends AppCompatActivity {
         super.onRestart();
         mCartCount = manager.cartTotalCount();
         setBadgeCount(this, mCartMenuIcon, String.valueOf(mCartCount));
+    }
+
+    private void top5ListData() {
+        String url = getString(R.string.server_url) + "/coffeeinfo/top5";
+        JsonArrayRequest socRequest = new JsonArrayRequest(Request.Method.GET, url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject coffee = response.getJSONObject(i);
+                        String korName = coffee.getString("korName");
+                        String sellCount = coffee.getString("sellCount") + "건";
+                        String img_name = coffee.getString("keyName");
+
+                        String packName = getApplication().getPackageName(); // 패키지명
+                        String coffeeImg = "@drawable/" + img_name;
+                        int coffeeImageResource = getResources().getIdentifier(coffeeImg, null, packName);
+
+                        int rankNumber = i;
+                        rankNumber++;
+                        String rankImage = "@drawable/rank" + rankNumber;
+                        int rankImageResource = getResources().getIdentifier(rankImage, null, packName);
+
+                        adapter.addItem(ContextCompat.getDrawable(getApplicationContext(), rankImageResource), ContextCompat.getDrawable(getApplicationContext(), coffeeImageResource),
+                                korName, sellCount) ;
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("ERROR" + error.getMessage());
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this.getApplicationContext());
+        requestQueue.add(socRequest);
+
     }
 
 
