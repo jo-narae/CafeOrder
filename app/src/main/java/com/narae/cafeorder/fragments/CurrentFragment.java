c2916590906ef705b326afe8895b399f20e889e4
jo-narae <nare6004@gmail.com>
2016-12-07 12:18:35 +0900

    주문현황 개발 완료

--- app/src/main/java/com/narae/cafeorder/fragments/CurrentFragment.java
+++ app/src/main/java/com/narae/cafeorder/fragments/CurrentFragment.java
@@ -5,7 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -16,7 +18,9 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.narae.cafeorder.R;
 import com.narae.cafeorder.adapter.CurrentListAdapter;
+import com.narae.cafeorder.database.UserDBManager;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -25,6 +29,7 @@ public class CurrentFragment extends Fragment{
 
     CurrentListAdapter adapter ;
     View currentView;
+    UserDBManager manager;
 
     public CurrentFragment() {
         // Required empty public constructor
@@ -41,11 +46,10 @@ public class CurrentFragment extends Fragment{
         // Inflate the layout for this fragment
         currentView = inflater.inflate(R.layout.fragment_current, container, false);
 
+        manager = new UserDBManager(getContext());
+
         // Adapter 생성 및 Adapter 지정.
         adapter = new CurrentListAdapter() ;
-
-        adapter.addItem("아메리카노", "Tall/Hot/2건/6000원") ;
-        adapter.addItem("카페라떼", "Tall/Iced/1건/4000원") ;
 
         ListView listview = (ListView)currentView.findViewById(R.id.statuslist);
         listview.setAdapter(adapter);
@@ -53,23 +57,50 @@ public class CurrentFragment extends Fragment{
         orderRequest();
 
         return currentView;
-
     }
 
     /**
      * 서버 통신 후 리스트 조회
      */
     private void orderRequest() {
-        String url = getString(R.string.server_url) + "/orders/current?userId=test";
+        String userId = manager.selectUserId();
+        String url = getString(R.string.server_url) + "/orders/current?userId=" + userId;
         JsonObjectRequest socRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                // 아이디가 있을 경우 이 로직 실행함
+                JSONArray jsonArray = null;
+                int totalCount = 0;
+                int totalPrice = 0;
+                // 리스트가 있을 경우 이 로직 실행함
+                try {
+                    jsonArray = response.getJSONArray("coffees");
+                    statusSetting(response.getString("orderStatus"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                totalCount = jsonArray.length();
+                for (int i = 0; i < totalCount; i++) {
+                    JSONObject coffee = null;
+                    try {
+                        coffee = jsonArray.getJSONObject(i);
+                        String keyName = coffee.getString("keyName");
+                        String detailString = coffee.getString("size") + "/" + coffee.getString("temperature") + "/" + coffee.getString("count") + "건/" + coffee.getString("totalPrice") + "원";
+                        totalPrice = totalPrice + coffee.getInt("totalPrice");
+                        adapter.addItem(keyName, detailString) ;
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                TextView totalCountText = (TextView) currentView.findViewById(R.id.totalCount);
+                TextView totalPriceText = (TextView) currentView.findViewById(R.id.totalPrice);
+
+                totalCountText.setText(totalCount + "건");
+                totalPriceText.setText(totalPrice + "원");
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                // 아이디가 없을 경우 이 로직 실행함
+                // 리스트가 없을 경우 이 로직 실행함
                 VolleyLog.d("error", error);
                 currentView.findViewById(R.id.currentResult).setVisibility(View.GONE);
                 currentView.findViewById(R.id.noCurrentResult).setVisibility(View.VISIBLE);
@@ -78,6 +109,32 @@ public class CurrentFragment extends Fragment{
 
         RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
         requestQueue.add(socRequest);
+    }
+
+    private void statusSetting(String orderStatus) {
+        String currentStatusText = "상태를 불러올 수 없습니다.";
+        if (orderStatus.equals("ORDER")) {
+            ImageView statusImg = (ImageView) currentView.findViewById(R.id.startbullet);
+            statusImg.setImageResource(R.drawable.startvisible);
+            currentStatusText = "주문이 신청되었습니다.";
+        }
+        if (orderStatus.equals("ACCEPT")) {
+            ImageView statusImg = (ImageView) currentView.findViewById(R.id.middlebullet1);
+            statusImg.setImageResource(R.drawable.middlevisible);
+            currentStatusText = "주문이 승인되었습니다.";
+        }
+        if (orderStatus.equals("MAKING")) {
+            ImageView statusImg = (ImageView) currentView.findViewById(R.id.middlebullet2);
+            statusImg.setImageResource(R.drawable.middlevisible);
+            currentStatusText = "음료를 제조하고 있습니다.";
+        }
+        if (orderStatus.equals("COMPLETE")) {
+            ImageView statusImg = (ImageView) currentView.findViewById(R.id.endbullet);
+            statusImg.setImageResource(R.drawable.endvisible);
+            currentStatusText = "주문이 완료되었습니다.";
+        }
+        TextView currentText = (TextView) currentView.findViewById(R.id.currentText);
+        currentText.setText(currentStatusText);
     }
 
 }
--- app/src/main/java/com/narae/cafeorder/fragments/HistoryFragment.java
+++ app/src/main/java/com/narae/cafeorder/fragments/HistoryFragment.java
@@ -8,10 +8,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.narae.cafeorder.R;
 import com.narae.cafeorder.adapter.HistoryListAdapter;
 import com.narae.cafeorder.history.History;
 import com.narae.cafeorder.history.HistoryItem;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,6 +32,8 @@ public class HistoryFragment extends Fragment{
     HistoryListAdapter listAdapter;
     ExpandableListView expListView;
     List<History> historyList;
+
+    View currentView;
 
     public HistoryFragment() {
         // Required empty public constructor
@@ -37,25 +48,27 @@ public class HistoryFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_history, container, false);
+        currentView = inflater.inflate(R.layout.fragment_history, container, false);
+
+        historyList = new ArrayList<>();
 
         // get the listview
-        expListView = (ExpandableListView) view.findViewById(R.id.hisExp);
+        expListView = (ExpandableListView) currentView.findViewById(R.id.hisExp);
 
-        prepareListDateNew();
+        //prepareListDateNew();
+        orderRequest();
         listAdapter = new HistoryListAdapter(getContext(), historyList);
 
         // setting list adapter
         expListView.setAdapter(listAdapter);
 
-        return view;
+        return currentView;
     }
 
     /*
      * Preparing the list data
      */
     private  void prepareListDateNew(){
-        historyList = new ArrayList<>();
         historyList.add(new History("0", ContextCompat.getDrawable(getContext(), R.drawable.americano), "주문완료", "아메리카노 외 1건"));
         historyList.add(new History("1", ContextCompat.getDrawable(getContext(), R.drawable.americano), "주문완료", "아메리카노"));
         historyList.add(new History("2", ContextCompat.getDrawable(getContext(), R.drawable.americano), "주문완료", "카페라떼 외 1건"));
@@ -69,6 +82,30 @@ public class HistoryFragment extends Fragment{
         historyList.get(2).addHistoryItem(new HistoryItem("아메리카노", "1", "3000"));
         historyList.get(2).addHistoryItem(new HistoryItem("카페라떼", "1", "3000"));
 
+    }
+
+    /**
+     * 서버 통신 후 리스트 조회
+     */
+    private void orderRequest() {
+        String url = getString(R.string.server_url) + "/orders/search_history?userId=test";
+        JsonObjectRequest socRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                // 아이디가 있을 경우 이 로직 실행함
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // 아이디가 없을 경우 이 로직 실행함
+                VolleyLog.d("error", error);
+                currentView.findViewById(R.id.hisExp).setVisibility(View.GONE);
+                currentView.findViewById(R.id.noCurrentResult).setVisibility(View.VISIBLE);
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        requestQueue.add(socRequest);
     }
 
 }
